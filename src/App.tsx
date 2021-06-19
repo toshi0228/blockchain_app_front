a01ee50b6636ecd1d8f202f0b0b7ff5c3f003964
toshi <etoshi0228@gmail.com>
2021-06-19 14:00:09 +0900

    Antdとstyled-componentを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import styled from "styled-components";
+import { Button } from "antd";
+import "./App.css";
+
+const CustomButton = styled(Button)`
+  background: pink;
+`;
+
 function App() {
   return (
     <div>
       <header></header>
-      <main>ブロックチェーンアプリ</main>
+      <main>
+        ブロックチェーンアプリ
+        <CustomButton>aa</CustomButton>
+      </main>
       <footer></footer>
     </div>
   );
